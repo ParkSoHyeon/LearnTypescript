82396c6d6ae3f1335ce4bc32a9c3496f9a4f5259
parksohyeon <dso753@elinmedia.com>
2020-10-27 22:45:17 +0900

    타입 모듈화 commit

---
+++ quiz/2_address-book/src/types.ts
@@ -0,0 +1,19 @@
+interface PhoneNumberDictionary {
+    [phone: string]: {
+        num: number;
+    };
+}
+
+interface Contact {
+    name: string;
+    address: string;
+    phones: PhoneNumberDictionary;
+}
+
+export enum PhoneType {
+    Home = 'home',
+    Office = 'office',
+    Studio = 'studio',
+}
+
+export { PhoneNumberDictionary, Contact }
